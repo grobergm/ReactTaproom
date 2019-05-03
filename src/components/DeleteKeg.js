1f73c6715ce9ba67e26ffd396f895c3ecb6ef635
Matt Groberg <grobergmg@gmail.com>
2019-05-03 16:11:16 -0700

    created button mixin to dry up code

--- src/components/DeleteKeg.js
+++ src/components/DeleteKeg.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 function DeleteKeg(){
-	const styles={
-		backgroundColor:'rgba(140,13,13)',
-		padding:'1vw',
-		borderRadius:'3px',
-		cursor:'pointer'
-	}
+
 	return(
-		<div style={styles}>
+		<div className='delete'>
 			Remove
 		</div>
 		)
