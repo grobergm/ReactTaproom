f5aa6dc36367fe8f27d26ab9f4cd5766527f902a
Matt Groberg <grobergmg@gmail.com>
2019-05-03 09:18:25 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Landing from './components/Landing';
+import Navbar from './components/Navbar';
+import Admin from './components/Admin';
+import KegList from './components/KegList';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route path='/admin' component={Admin} />
+          <Route path='/keglist' component={KegList} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+     </Router>
   );
 }
 
---
+++ src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AddKeg(){
+	return(
+		<div>
+		</div>
+		)
+}
+
+export default AddKeg;
