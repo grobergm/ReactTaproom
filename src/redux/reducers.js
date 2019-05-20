af281d06da2bf9b2444ff5f7f4b1f6eb0c2266eb
Matt Groberg <grobergmg@gmail.com>
2019-05-20 12:43:45 -0700

    add reducers and tests for adding, deleting, and updating pints in kegs

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -16,10 +16,15 @@ const kegReducer=(state,action)=>{
 }
 
 
-const kegListReducer=(state=[],action)=>{
+const kegListReducer=(state,action)=>{
 	switch(action.type){
 		case 'POUR_DRINK':
 			return state.map(k=>kegReducer(k,action))
+		case 'ADD_KEG':
+			return state.concat(action.keg);
+		case 'DELETE_KEG':
+			const index=state.findIndex(k=>{ return k.id===action.id});
+			return state.slice(0,index).concat(state.slice(index+1));
 		default:
 			return state;
 	}
--- src/redux/reducers.test.js
+++ src/redux/reducers.test.js
@@ -1,7 +1,93 @@
 import kegListReducer from './reducers';
+import kegArray from '../models/kegData';
 
 describe('kegListReducer',()=>{
+	const initialState=[
+		{
+			id:'asdasdsadeawe',
+			pints:0
+		},
+		{
+			id:'qpowpopowpwop',
+			pints:0
+		},
+		{
+			id:'dfsgdsfgdfsg',
+			pints:0
+		},
+	]
 	test('Should return initial state if given unknown action',()=>{
-		expect(kegListReducer([],{type:null})).toEqual([]);
+		expect(kegListReducer(initialState,{type:null})).toEqual(initialState);
+	});
+
+	test('should increment pints for matching keg id',()=>{
+		const nextState=[
+			{
+				id:'asdasdsadeawe',
+				pints:0
+			},
+			{
+				id:'qpowpopowpwop',
+				pints:0
+			},
+			{
+				id:'dfsgdsfgdfsg',
+				pints:1
+			},
+		];
+		const action={
+			type:'POUR_DRINK',
+			id:'dfsgdsfgdfsg'
+		}
+		expect(kegListReducer(initialState,action)).toEqual(nextState);
+	});
+
+	test('Should add a keg object to the initial state',()=>{
+
+		const action={
+			type:'ADD_KEG',
+			keg:{
+				id:'oiiowioiiow',
+				pints:0
+			}
+		}
+		const nextState=
+		[
+			{
+				id:'asdasdsadeawe',
+				pints:0
+			},
+			{
+				id:'qpowpopowpwop',
+				pints:0
+			},
+			{
+				id:'dfsgdsfgdfsg',
+				pints:0
+			},
+			{
+				id:'oiiowioiiow',
+				pints:0
+			}
+		]
+		expect(kegListReducer(initialState,action)).toEqual(nextState);
+	});
+
+	test('should remove keg with matching action id',()=>{
+		const action={
+			type:'DELETE_KEG',
+			id:'qpowpopowpwop'
+		}
+		const nextState=[
+			{
+				id:'asdasdsadeawe',
+				pints:0
+			},
+			{
+				id:'dfsgdsfgdfsg',
+				pints:0
+			},
+		];
+		expect(kegListReducer(initialState,action)).toEqual(nextState);
 	})
 })
