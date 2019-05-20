b781dc832b7e293d7cb3dac1b69f89e9bb424ae6
Matt Groberg <grobergmg@gmail.com>
2019-05-20 13:47:06 -0700

    connect masterKegList redux store and refactor

--- src/App.js
+++ src/App.js
@@ -1,74 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from 'react-redux';
 
 import Landing from './components/Landing';
 import Staff from './components/staff/Staff';
 import Taproom from './components/customers/Taproom';
 import NotFound from './components/NotFound';
-
 import Navbar from './components/Navbar';
 
-import kegArray from './models/kegData';
 
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      kegs:[],
-    }
-    this.handlePourDrink=this.handlePourDrink.bind(this);
-    this.handleDeleteKeg=this.handleDeleteKeg.bind(this);
-    this.handleAddNewKeg=this.handleAddNewKeg.bind(this);
-  }
- 
-  componentDidMount(){
-    this.setState({kegs:kegArray});
-  }
-
-  handlePourDrink(drinkID){
-    let newKegs=this.state.kegs.slice();
-    let selectedKegIndex=this.state.kegs.findIndex(keg=>{return keg.id===drinkID});
-    newKegs[selectedKegIndex].pints++;
-    this.setState({kegs:newKegs});
-  }
-
-  handleDeleteKeg(drinkID){
-    let newKegs=this.state.kegs.slice();
-    let selectedKegIndex=this.state.kegs.findIndex(keg=>{return keg.id===drinkID});
-    newKegs.splice(selectedKegIndex,1);
-    this.setState({kegs:newKegs});
-  }
-
-  handleAddNewKeg(keg){
-    let newKegs=this.state.kegs.slice();
-    newKegs.push(keg);
-    this.setState({kegs:newKegs})
-  }
-
-  render(){
-    console.log(this.props)
-    return (
-     <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route 
-            path='/staff' 
-            render={()=><Staff 
-              kegs={this.state.kegs} 
-              onPourDrink={this.handlePourDrink} 
-              onDeleteKeg={this.handleDeleteKeg} 
-              onAddNewKeg={this.handleAddNewKeg} />
-            } />
-          <Route path='/taproom' render={()=><Taproom kegs={this.state.kegs}/>} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-     </Router>
+function App(props){
+  return (
+   <Router>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route 
+          path='/staff' 
+          render={()=><Staff kegs={props.masterKegList} />
+          } />
+        <Route path='/taproom' render={()=><Taproom kegs={props.masterKegList} />} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+   </Router>
   );
+}
+
+const mapStateToProps=state=>{
+  return {
+    masterKegList:state
   }
 }
 
-export default App;
+
+export default connect(mapStateToProps)(App);
