f148d2c0ac68393186f05563b629b8b6bef68e1b
Matt Groberg <grobergmg@gmail.com>
2019-05-20 20:17:26 -0700

    tabreducer ADD_DRINK_TO_TAB action with test

--- src/redux/tabReducers.js
+++ src/redux/tabReducers.js
@@ -1,7 +1,25 @@
+const tabReducer=(state,action)=>{
+	switch(action.type){
+		case 'ADD_TO_ORDER':
+			if(state.id!==action.id){
+				return state
+			}
+				return {
+					...state,
+					order: state.order.concat(action.drink)
+				}
+		default:
+			return state
+	}
+}
+
+
 const tabListReducer=(state,action)=>{
 	switch(action.type){
 		case 'ADD_TAB':
-		 return state.concat(action.tab)
+			return state.concat(action.tab)
+		case 'ADD_TO_ORDER':
+			return state.map(t=>tabReducer(t,action))
 		default:
 			return state;
 	}
--- src/redux/tabReducers.test.js
+++ src/redux/tabReducers.test.js
@@ -1,13 +1,11 @@
 import tabListReducer from './tabReducers';
 
 describe('tabReducer',()=>{
-	const initialState=
-	[
-		{
-			id:'aoijijasdoijo',
-			order:[]
-		}
-	]
+	const initialState=[
+		{id:'aoijijasdoijo',order:[]},
+		{id:'aasdsadsdsdasijo',order:[]}
+	];
+
 	test('it returns initial state if unknown action',()=>{
 		expect(tabListReducer(initialState,{type:null})).toEqual(initialState)
 	})
@@ -19,7 +17,21 @@ describe('tabReducer',()=>{
 		};
 		const nextState=[
 			{id:'aoijijasdoijo',order:[]},
+			{id:'aasdsadsdsdasijo',order:[]},
 			{id:'lasdlklaskd',order:[]}
+		]
+		expect(tabListReducer(initialState,action)).toEqual(nextState);
+	})
+
+	test('add drink to selected tab order',()=>{
+		const action={
+			type:'ADD_TO_ORDER',
+			id:'aasdsadsdsdasijo',
+			drink:{name:'sleigher',price:4}
+		}
+		const nextState=[
+			{id:'aoijijasdoijo',order:[]},
+			{id:'aasdsadsdsdasijo',order:[{name:'sleigher',price:4}]}
 		]
 		expect(tabListReducer(initialState,action)).toEqual(nextState);
 	})
